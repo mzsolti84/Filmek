d2a891c8c606ab36ccc66d9a902b1b60b3b542df
Molnár Zsolt <molnar.zsolt.1984@gmail.com>
2021-11-17 21:33:45 +0100

    MZs házi OOP 2

--- src/main/java/hu/progmatic/Videoteka.java
+++ src/main/java/hu/progmatic/Videoteka.java
@@ -1,4 +1,17 @@
 package hu.progmatic;
 
 public class Videoteka {
+  private Film[] filmek;
+  private int filmDb;
+  private final int size;
+
+  public Videoteka (int size) {
+    Film [] filmek = new Film[size];
+    this.size = size;
+  }
+
+  public void addFilm(Film film) {
+    if (this.filmDb > this.size) { throw new RuntimeException(); }
+    if (film != null) filmek[filmDb++] = film;
+  }
 }
